413abf16339b875255d688f3f0823d192bc41343
IcyAltair <muratov.syu@edu.spbstu.ru>
2025-03-16 20:43:30 +0300

    #44834199 from df to np

--- gamac/src/tests/k_means_tests.py
+++ gamac/src/tests/k_means_tests.py
@@ -1,10 +1,11 @@
+import os
+import pandas as pd
 import sys
-
 from sklearn.datasets import make_blobs
 
 sys.path.append('../')
 from algorithms.kmeans_cpu import KMeans
-from algorithms.kmeans_gpu import KMeansGpu
+from algorithms.kmeans_gpu_cupy import KMeansGPU
 
 def cpu_test():
     # Generate synthetic data
@@ -16,13 +17,12 @@ def cpu_test():
     print(y_pred)
 
 def gpu_test():
-    # Generate synthetic data
-    X, _ = make_blobs(n_samples=50, centers=3, cluster_std=0.6)
-    
+    X = pd.read_parquet('data\cifar_embs.parquet', engine='pyarrow').to_numpy()
     batch_size = X.shape[0]
     threads_per_block = 256
-    blocks_per_grid = (batch_size + threads_per_block - 1) // threads_per_block
-    y_pred = KMeansGpu.predict[blocks_per_grid, threads_per_block](X)
+    # blocks_per_grid = (batch_size + threads_per_block - 1) // threads_per_block
+    clf = KMeansGPU(k=3)
+    y_pred = clf.predict(X)
 
     print(y_pred)
 
