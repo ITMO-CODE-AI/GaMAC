6a55a625fa3410f052def24022ac8daa49ec98f7
IcyAltair <muratov.syu@edu.spbstu.ru>
2025-06-06 02:13:31 +0300

    #49692926 minor changes

--- test.py
+++ test.py
@@ -12,7 +12,10 @@ TARGET_MEASURES = os.getenv('TARGET_MEASURES', "BR,OS,MCR,SYM")
 def main():
     measures = {"BR": Internal.BR, "OS": Internal.OS, "MCR": Internal.MCR, "SYM": Internal.SYM}
     used_measures = [measures[x] for x in TARGET_MEASURES.split(sep=',')]
-    data = pd.read_csv(f'test-data/{DATA}')
+    if '.csv' in DATA:
+        data = pd.read_csv(f'test-data/{DATA}')
+    else:
+        pd.read_parquet(f'test-data/{DATA}')
     data = data.drop("class", errors='ignore', axis=1)
     df, optimal = Gamac(target_measures=tuple(used_measures)).run(table=data, text=None, image=None)
     #df, optimal = Gamac().run(table=data, text=None, image=None)
