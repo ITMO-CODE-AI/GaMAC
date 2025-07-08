32f3d74c1b87446878e44db0660b5bdb740ed07f
IcyAltair <muratov.syu@edu.spbstu.ru>
2025-07-08 21:31:11 +0300

    #52254516 dropped openssl

--- devops/download_minio.py
+++ devops/download_minio.py
@@ -1,16 +1,16 @@
 from minio import Minio
 from os import environ, getenv
 
-
 MINIO_ACCESS_KEY = getenv('MINIO_ACCESS_KEY')
 MINIO_SECRET_KEY = getenv('MINIO_SECRET_KEY')
 MINIO_API_HOST = getenv('MINIO_ENDPOINT')
+MINIO_BUCKET_URL = getenv('MINIO_BUCKET_URL')
 DATA = getenv('DATA')
 print(getenv('SSL_CERT_FILE'))
 # environ["SSL_CERT_FILE"] = getenv('MINIO_CERT_PATH')
 BUCKET = 'datasets'
 
-def download_data():
+def download_data_with_creds():
     client = Minio(
         MINIO_API_HOST,
         access_key=MINIO_ACCESS_KEY,
@@ -25,5 +25,17 @@ def download_data():
     )
     return
 
-download_data()
+def download_data_without_creds():
+    client = Minio(
+    MINIO_API_HOST,
+    secure=True
+    )
+    data = client.fget_object(
+        BUCKET,
+        f'data/{DATA}',
+        f'test-data/{DATA}'
+    )
+
+download_data_without_creds()
+
 
