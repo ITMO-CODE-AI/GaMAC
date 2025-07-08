c714d9301a513ad89da482433d333d42c9dba173
IcyAltair <muratov.syu@edu.spbstu.ru>
2025-07-08 19:41:41 +0300

    #52254516 fixed path bug in tests

--- devops/test.py
+++ devops/test.py
@@ -1,6 +1,9 @@
 import time
 import os
 import pandas as pd
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from gamac.autoclustering import Gamac
 from gamac.estimation.internal import Internal
